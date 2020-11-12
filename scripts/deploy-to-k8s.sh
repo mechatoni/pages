455a306b6d777ac28b3a0f41e88f71ccaa147e32
mechatoni <mechatoni@users.noreply.github.com>
2020-11-12 14:41:20 +0100

    Fix pv and pvc references

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-kubectl apply -f deployment/mysql/mysql-pv.yaml
-kubectl apply -f deployment/mysql/mysql-pvc.yaml
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
 
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
